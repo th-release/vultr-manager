eeccc7f2f8e696c409d7b5eb416854097bdf81a7
th-release <cth.release@gmail.com>
2025-10-21 17:56:09 +0900

    [update] api bug fix

--- api/server.go
+++ api/server.go
@@ -118,7 +118,7 @@ func (s *ServerConfig) setupRoutes() {
 
 	instanceGroup := apiGroup.Group("/instance", InstanceMiddleware)
 	instanceGroup.Get("/list", instance.List)
-	instanceGroup.Get("/detail/:uuid", instance.DatabaseDetail)
+	instanceGroup.Get("/detail/:uuid", instance.Detail)
 	instanceGroup.Get("/database/list", instance.DatabaseList)
 	instanceGroup.Get("/database/detail/:uuid", instance.DatabaseDetail)
 	instanceGroup.Post("/create", instance.Create)
