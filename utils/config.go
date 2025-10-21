082ee9aa54c5b07047a93b9ce5c3eb294aa22b30
th-release <cth.release@gmail.com>
2025-10-21 17:06:27 +0900

    [update] releease

--- utils/config.go
+++ utils/config.go
@@ -15,7 +15,7 @@ func GetConfig() *Config {
 	return &Config{
 		ApiKey:           ThreeTermString(len(os.Getenv("API")) >= 0, os.Getenv("API"), "APIKEY"),
 		Password:         ThreeTermString(len(os.Getenv("PASSWORD")) >= 0, os.Getenv("API"), "PASSWORD"),
-		Port:             ThreeTermString(len(os.Getenv("PORT")) >= 0, os.Getenv("PORT"), "3000"),
+		Port:             ThreeTermString(len(os.Getenv("PORT")) >= 0, os.Getenv("PORT"), "8080"),
 		DatabaseAddr:     ThreeTermString(len(os.Getenv("DATABASE_ADDR")) >= 0, os.Getenv("DATABASE_ADDR"), "localhost"),
 		DatabaseUser:     ThreeTermString(len(os.Getenv("DATABASE_USER")) >= 0, os.Getenv("DATABASE_USER"), "vultr"),
 		DatabasePassword: ThreeTermString(len(os.Getenv("DATABASE_PASSWORD")) >= 0, os.Getenv("DATABASE_PASSWORD"), "vultr"),
