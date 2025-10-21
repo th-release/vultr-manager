ff1a3ee09e2ea5ad65891dcf3e77a95934889e9f
th-release <cth.release@gmail.com>
2025-10-21 17:37:21 +0900

    [update] typo fix

--- utils/config.go
+++ utils/config.go
@@ -14,7 +14,7 @@ func GetConfig() *Config {
 
 	return &Config{
 		ApiKey:           ThreeTermString(len(os.Getenv("API")) >= 0, os.Getenv("API"), "APIKEY"),
-		Password:         ThreeTermString(len(os.Getenv("PASSWORD")) >= 0, os.Getenv("API"), "PASSWORD"),
+		Password:         ThreeTermString(len(os.Getenv("PASSWORD")) >= 0, os.Getenv("PASSWORD"), "PASSWORD"),
 		Port:             ThreeTermString(len(os.Getenv("PORT")) >= 0, os.Getenv("PORT"), "8080"),
 		DatabaseAddr:     ThreeTermString(len(os.Getenv("DATABASE_ADDR")) >= 0, os.Getenv("DATABASE_ADDR"), "localhost"),
 		DatabaseUser:     ThreeTermString(len(os.Getenv("DATABASE_USER")) >= 0, os.Getenv("DATABASE_USER"), "vultr"),
